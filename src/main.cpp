887b726f4e69df9850d30b68a75a2bdd20d424e0
Daniel Santos <daninsky12@gmail.com>
2024-03-14 01:10:57 -0300

    Define mapping convention

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,40 @@
 #include <vector>
 #include <type_traits>
 #include <cstdint>
+#include <string.h>
 #include <sqlite3.h>
+
+class Database {
+    // TODO: Detalhes de implementação do banco de dados
+public:
+    Database() {
+        int err = sqlite3_open(filename, db_handle);
+        if (err) {
+            exit(1);
+        }
+    }
+    
+    void execute(const char* sql_stmt) {
+        sqlite3_stmt**  stmt;
+        const char** tail;
+        sqlite3_prepare(*db_handle, sql_stmt, strlen(sql_stmt), stmt, tail);
+        if (!SQLITE_OK) {
+            std::cerr << "Erro ao executar SQL.\n";
+            return;
+        }
+        sqlite3_prepare(*db_handle);
+    }
+    
+    static sqlite3** db_hanle
+    constexpr static const char* filename = "Car_Database.db";
+};
 
 enum ColumnType {
     INTEGER,
     VARCHAR
 };
 
-
-class ColumnInterface {
-public:
+struct ColumnInterface {
     std::string name;
     ColumnType type;
     int size;
@@ -19,18 +43,13 @@ public:
     int scale;
     bool primary_key;
     bool not_null;
-
-    ColumnInterface(std::string n, ColumnType t)
-        : name{n}
-        , type{t} {
-    }
 };
 
 template<typename T>
-class Column : public ColumnInterface {
+struct Column : ColumnInterface {
 public:
     Column(std::string n, ColumnType t)
-        : ColumnInterface(n, t) {
+        : ColumnInterface{.name = n, .type = t} {
     }
     T get_value() {
         return value;
@@ -46,38 +65,51 @@ private:
 
 class Entity {
 public:
-    virtual std::vector<ColumnInterface*> get_columns() = 0;
+    Entity(std::string name) : table_name{name} {}
+    std::string table_name;
 };
 
+// TODO: Adicionar validação de colunas repetidas
 class Custumer : public Entity {
+public:
+    Custumer()
+    : Entity{"Custumer"} { }
     Column<int> custumer_id{"custumer_id", ColumnType::INTEGER};
     Column<std::string> first_name{"first_name", ColumnType::VARCHAR};
-    
-    std::vector<ColumnInterface*> get_columns() override {
-        std::vector<ColumnInterface*> r;
-        r.push_back(&custumer_id);
-        r.push_back(&first_name);
-        return r;
-    }
+    Column<std::string> last_name{"last_name", ColumnType::VARCHAR};
+    Column<std::string> gender{"gender", ColumnType::VARCHAR};
+    Column<int> house_income{"house_income", ColumnType::INTEGER};
+    Column<std::string> email{"email", ColumnType::INTEGER};
+    std::vector<ColumnInterface*> columns{
+        &custumer_id,
+        &first_name
+    };
 };
 
 template<typename T>
 class Repository {
+    static_assert(std::is_base_of<Entity, T>::value, "Repository require an Entity type");
 public:
-    Repository() { }
-    
-};
-
-template<uint32_t N>
-struct binary {
-    static uint32_t const value = binary<N/10>::value << 1 | N%10;
-};
-
-template<>
-struct binary<0> {
-    static uint32_t const value = 0;
+    std::string findById() {
+        std::string stmt = "SELECT * FROM " + entity.table_name + ';';
+        return stmt;
+    }
+    std::string findBy(ColumnInterface column) {
+        std::string stmt = "SELECT " + column.name + " FROM " + entity.table_name +
+            ';';
+        return stmt;
+    }
+    T entity;
+    static Database database;
 };
 
 int main() {
-    std::cout << binary<10>::value;
+    Custumer custumer;
+    Repository<Custumer> custumer_dao;
+    std::cout << custumer_dao.findById() << '\n';
+    std::cout << custumer_dao.findBy(custumer.first_name) << '\n';
+    std::cout << custumer_dao.findBy(custumer.last_name) << '\n';
+    for (auto column : custumer.columns) {
+        std::cout << column->name << '\n';
+    }
 }
